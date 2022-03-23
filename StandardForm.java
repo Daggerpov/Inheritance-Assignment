e0361b16a4d9ee1711aa9d2d750725650ecf760b
Daggerpov <danielagapov1@gmail.com>
2022-03-22 20:37:46 -0400

    starting to comment

--- StandardForm.java
+++ StandardForm.java
@@ -12,11 +12,17 @@ public class StandardForm extends Quadratic {
 
 		String factoredFormOfEquation = "";
 
+		// if the numbers are invalid, that means I can't use them as factors
 		if (factors.get(0).isNaN() == true && factors.get(1).isNaN() == true){
 			return "This equation cannot be converted to factored form since it has no factors";
 		} else {
+			// starting to bulid out the equation string with the a value since it's at the start
 			factoredFormOfEquation = Integer.toString(getAVal());
 			
+			// if there is an invalid factor then it won't be considered
+			// otherwise, it will be included as a factor and the sign is inversed
+			// due to the (x - s) and (x - t) format
+
 			if (factors.get(0).isNaN() == false){
 				factoredFormOfEquation += "(x ";
 				if (factors.get(0) >= 0){
@@ -24,6 +30,8 @@ public class StandardForm extends Quadratic {
 				} else {
 					factoredFormOfEquation += "+ ";
 				}
+				// after flipping its sign, I then append the absolute value since I've
+				// already added the sign
 				factoredFormOfEquation += Double.toString(Math.abs(factors.get(0))) + ")";
 			}
 
